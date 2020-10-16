e56b9812f70925cbf0a662f9058b44dddf36bd49
Mario Nogueira <48410549+supramatti@users.noreply.github.com>
2020-10-15 21:51:01 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using exec_cap09.Entities;
+using exec_cap09.Entities.Enums;
+using System;
+
+namespace exec_cap09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birth = DateTime.Parse(Console.ReadLine());
+            Client client = new Client(name, email, birth);
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Order order = new Order(DateTime.Now, status, client);
+            Console.WriteLine("How many itens to this order? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Enter {i+1}# item data:");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double productPrice = double.Parse(Console.ReadLine());
+                Product product = new Product(productName, productPrice);
+                Console.Write("Quantity: ");
+                int productQuantity = int.Parse(Console.ReadLine());
+                OrderItem orderItem = new OrderItem(productQuantity, product);
+                order.AddItem(orderItem);
+            }
+            Console.WriteLine(order);
+        }
+    }
+}
